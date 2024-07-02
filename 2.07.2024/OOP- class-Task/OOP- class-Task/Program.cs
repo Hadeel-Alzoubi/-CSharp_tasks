8d3869c486d87be3709f3c8eff68d2ee622cfbef
Hadeel-Alzoubi <152480249+Hadeel-Alzoubi@users.noreply.github.com>
2024-07-02 20:01:42 +0300

    Add files via upload

---
+++ 2.07.2024/OOP- class-Task/OOP- class-Task/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP__class_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BMW bmw = new BMW();
+            Console.WriteLine(bmw.Display());
+            Car car = new Car();
+            Console.WriteLine(car.Display());
+            Car bmwcar = new BMW();
+            Console.WriteLine(bmwcar.Display());
+            Console.ReadKey();
+        }
+    }
+
+   public class Car
+    {
+        int make;
+        int year;
+        string type;
+        float price;
+        int model;
+        int palletNo;
+        string color;
+
+        public void DisplayInfo(int make,int year)
+        {
+
+        }
+        public void DisplayInfo(int make)
+        {
+
+        }
+        public virtual string Display()
+        {
+            return "Welcome Car";
+        }
+        //public  string Display()
+        //{
+        //    return "Welcome Car";
+        //}
+
+    }
+
+    public class BMW :Car
+    {
+        public override string Display()
+        {
+            return "welcome BMW";
+        }
+        //public string Display()
+        //{
+        //    return "welcome BMW";
+        //}
+    }
+}
---
+++ 2.07.2024/Task2/Task2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Cat cat = new Cat("cat");
+            cat.MakeSound();
+            cat.Sleep();
+            cat.Eat();
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("-------------------------------------");
+
+            Dog dog = new Dog("dog");
+            dog.MakeSound();
+            dog.Sleep();
+            dog.Eat();
+            Console.ReadKey();
+        }
+    }
+
+    interface ISound
+    {
+        void MakeSound();
+    }
+
+    abstract class Animal
+    {
+        private string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public abstract void Eat();
+
+       public void Sleep()
+        {
+            Console.WriteLine($"{Name} is Sleeping ");
+        }
+
+    }
+
+    class Dog : Animal, ISound
+    {
+        public Dog(string name) : base(name)
+        {
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("The Dog Eat bone .");
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("The Dog Sound Ho Ho .");
+        }
+    }
+
+    class Cat : Animal , ISound
+    {
+        public Cat(string name) : base(name)
+        {
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("The Cat Eat meat .");
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("The Cat Sound Mew Mew .");
+        }
+    }
+}
